95d0b934a29ca42ca774df865ed21503dfda51f4
johnnyjuarez <johnnyjuarez1992@gmail.com>
2020-10-22 10:10:30 -0500

    set up app.js for basic express

---
+++ src/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+
+// describe app to use express
+const app = express();
+
+// set morgan option based on node environment
+const morganOption = (process.env.NODE_ENV === 'production') ? 'tiny' : 'common';
+
+// set app options and basic security
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+module.exports = app;
