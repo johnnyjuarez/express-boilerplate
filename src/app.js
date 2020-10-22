1f107442b7114067fb7f572b3056c6f3e0309ead
johnnyjuarez <johnnyjuarez1992@gmail.com>
2020-10-22 10:27:43 -0500

    added error handler middleware

--- src/app.js
+++ src/app.js
@@ -19,4 +19,15 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error : {mesage : 'server error'}};
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
 module.exports = app;
